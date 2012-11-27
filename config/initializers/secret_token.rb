2fc1f8e06a5dd8864d0d5fedb8525ab6b976f550
Yuta Takahashi <m1205123ing@gmail.com>
2012-11-28 08:56:02 +0900

    make hatenaPrj

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HatenaPrj::Application.config.secret_token = '64b8ac08878ec88fa01f7099b118c2cd6bd4cfde2ab4ca422ac6eb0a842f1368fe112b33335dbc3fa73b7b22b88d408fb560bb2edab039dae4a6022900aa9041'
