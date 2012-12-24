512c8baf485d80ca189fc4a398a91e74686f51d0
yuta takahashi <m1205123ing@gmail.com>
2012-12-24 17:02:13 +0900

    URLが正しく認識されるよう修正。

--- app/controllers/hatena_controller.rb
+++ app/controllers/hatena_controller.rb
@@ -2,39 +2,50 @@
 
 class HatenaController < ApplicationController
 
+  API_URL = "http://b.hatena.ne.jp/entry/json/"
+
   def search
     require 'open-uri'
     require 'json'
-    
+
+    parsed=['']
     @url = params[:url]
-
-    if @url.blank?
-      @url = ""
-      return
-    end
-
-    html = open(@url).read
-    json = JSON.parser.new(html)
-
-    @users = html
-
-    #parse()
-    hash = json.parse()
-    parsed = hash['bookmarks']
-    @title = hash['title']
-    @entry_url = hash['entry_url']
-    
     @timestamps = Array.new(parsed.length)
     @comments = Array.new(parsed.length)
     @users = Array.new(parsed.length)
     @tags = Array.new(parsed.length)
 
+    if @url.blank?
+      @url = ""
+      @alert = "URLを入力してください。"
+      return
+    end
+
+    # 入力されたURLをJSONで展開する
+    begin
+      html = open(API_URL+@url).read
+      json = JSON.parser.new(html)
+      hash=json.parse()
+
+    rescue
+      @alert="URLが間違っています。\n正しく入力してください。"
+      return
+    end
+
+    if hash.present?
+      parsed = hash['bookmarks']
+      @title = hash['title']
+      @entry_url = hash['entry_url']
+    else
+      @alert="データがありません。"
+      return
+    end
+    
     parsed.each_with_index do |item, i|
       @timestamps[i] = item['timestamp']
       @comments[i] = item['comment']
       @users[i] = item['user']
       @tags[i] = item['tags']
     end
-
   end
 end
